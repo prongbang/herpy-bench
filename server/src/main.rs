d75a42d72291e1c19a107582249f9384d6e9f63d
prongbang <prongbang@gmail.com>
2025-10-18 16:51:26 +0700

    feat: add route by method

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, Result, App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{
+    delete, get, patch, post, put, web, App, HttpResponse, HttpServer, Responder, Result,
+};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -19,14 +21,59 @@ async fn hello() -> Result<impl Responder> {
     Ok(web::Json(obj))
 }
 
+#[get("/get")]
+async fn get() -> Result<impl Responder> {
+    let obj = MyObj {
+        name: "Hello World!".to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[post("/post")]
+async fn post() -> Result<impl Responder> {
+    let obj = MyObj {
+        name: "Hello World!".to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[put("/put")]
+async fn put() -> Result<impl Responder> {
+    let obj = MyObj {
+        name: "Hello World!".to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[patch("/patch")]
+async fn patch() -> Result<impl Responder> {
+    let obj = MyObj {
+        name: "Hello World!".to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[delete("/delete")]
+async fn delete() -> Result<impl Responder> {
+    let obj = MyObj {
+        name: "Hello World!".to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(hello)
+            .service(get)
+            .service(post)
+            .service(put)
+            .service(patch)
+            .service(delete)
     })
-        .bind(("127.0.0.1", 8000))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
